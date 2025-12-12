7bd3ddeedc5a4a2971e30fbc1835a07dc2aa0343
eronfelix-js <eronf5594@gmail.com>
2025-12-12 16:30:54 -0300

    fix: correção de teste por erro de workflow

--- src/test/java/dev/felix/event_api/EventApiApplicationTests.java
+++ src/test/java/dev/felix/event_api/EventApiApplicationTests.java
@@ -1,13 +1,8 @@
 package dev.felix.event_api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EventApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
